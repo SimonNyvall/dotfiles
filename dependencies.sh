6100869d8bc02320c082a227180e4fcc9c3307ad
Simon Nyvall <simon.k.nyvall@proton.me>
2024-01-07 14:01:38 +0100

    Add installs

--- dependencies.sh
+++ dependencies.sh
@@ -28,3 +28,12 @@ sudo apt install fzf -y
 # Install alacritty
 sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt install alacritty -y
+
+# Install lazygit
+LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
+curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+tar xf lazygit.tar.gz lazygit
+sudo install lazygit /usr/local/bin
+
+# Install font
+echo 'Install the font at https://github.com/ryanoasis/nerd-fonts/releases/download/v3.1.1/JetBrainsMono.zip'
