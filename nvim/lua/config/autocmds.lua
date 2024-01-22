65b3e07aa5741f6f67070e690bc672942d1749a6
SimonNyvall <simon.nyvall@captario.com>
2024-01-22 16:27:15 +0100

    Fix the dap error in neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,1 +1,15 @@
+local dap_text = require("nvim-dap-virtual-text")
 
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "fsharp",
+  callback = function()
+    dap_text.disable()
+  end,
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "cs",
+  callback = function()
+    dap_text.disable()
+  end,
+})
