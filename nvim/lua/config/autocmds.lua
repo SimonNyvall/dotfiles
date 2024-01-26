69b2f9a5021520532208c52e3eea7a01f0663db6
SimonNyvall <simon.nyvall@captario.com>
2024-01-26 08:55:46 +0100

    Update fsharp highlighting and type signeture

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -4,6 +4,8 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "fsharp",
   callback = function()
     dap_text.disable()
+    vim.g["fsharp#backend"] = "disable"
+    vim.g["fsharp#show_signature_on_cursor_move"] = 1
   end,
 })
 
--- nvim/lua/plugins/fsharp.lua
+++
@@ -1,3 +0,0 @@
-return {
-  "DrTom/fsharp-vim",
-}
