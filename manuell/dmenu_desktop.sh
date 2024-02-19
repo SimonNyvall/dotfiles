e8e6168de5e7ab0614898253c4479c644ed1e683
SimonNyvall <simon.k.nyvall@proton.me>
2024-02-19 18:10:18 +0100

    Update dotfiles

---
+++ manuell/dmenu_desktop.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# You can add options here. For example, to set font and color:
+# dmenu_run -fn 'Monospace-10' -nb '#222222' -nf '#bbbbbb' -sb '#005577' -sf '#eeeeee'
+
+# This runs dmenu without any customization other than specifying a prompt:
+dmenu_run -p 'Run: '
