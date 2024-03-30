ed8d10b0741af297ca172c5bbe2cbc4ee843c92d
SimonNyvall <simon.k.nyvall@proton.me>
2024-03-30 18:20:19 +0100

    work in progress: .

--- nvim/lua/plugins/lazyplugs.lua
+++ nvim/lua/plugins/lazyplugs.lua
@@ -205,6 +205,10 @@ return {
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
   { import = "lazyvim.plugins.extras.lang.json" },
 
+  { import = "lazyvim.plugins.extras.lang.omnisharp" },
+
+  { import = "lazyvim.plugins.extras.test.core" },
+
   -- add any tools you want to have installed below
   {
     "williamboman/mason.nvim",
