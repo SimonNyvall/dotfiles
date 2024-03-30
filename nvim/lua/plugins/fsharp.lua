6b3fa1a0cb573b88f1c841f91e0672c0ef74acf3
SimonNyvall <simon.k.nyvall@proton.me>
2024-03-30 01:54:16 +0100

    work in progress: .

--- nvim/lua/plugins/fsharp.lua
+++ nvim/lua/plugins/fsharp.lua
@@ -1,3 +1,16 @@
 return {
-  "SimonNyvall/fsharp-vim",
+  {
+    -- fsharp text highlighting
+    "SimonNyvall/fsharp-vim",
+  },
+
+  {
+    -- ionide-fsharp
+    "ionide/Ionide-vim",
+    config = function()
+      vim.g["fsharp#lsp_recommended_colorscheme"] = 0
+      vim.g["fsharp#linter"] = 1
+      vim.g["fsharp#unused_opens_analyzer"] = 1
+    end,
+  },
 }
